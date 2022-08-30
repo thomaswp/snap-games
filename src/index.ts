842b371cf7272a6da67fd6fdae0a7891b416da02
Thomas Price <twprice@ncsu.edu>
2022-08-30 10:26:55 -0400

    Test config for extension using sef

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// import  {BlockFactory} from '../sef/src/blocks/BlockFactory'
+
+import { EventManager, SnapHelper } from 'sef';
+
+console.log("!!!!");
+
+export const test = 12;
+
+const em = new EventManager();
+em.test();
+
+const sh = new SnapHelper();
+console.log('IDE', sh.snap().IDE());
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+//webpack.config.js
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, './sef/snap/src/sef-libraries/'),
+    filename: "games.js", // <--- Will be compiled to this single file
+    library: "SnapGames",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }
+    ]
+  },
+  externals: {
+    sef: 'SEF',
+  },
+};
