82f8972de278fda2cbce1e9de2f5db2189701bf9
Thomas Price <twprice@ncsu.edu>
2022-09-01 14:42:56 -0400

    Testing adding blocks

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import { Events, Extension } from 'sef';
+import { Blocks, Events, Extension, Snap } from 'sef';
+import { Color } from 'sef/src/snap/Snap';
 
 export class SnapGames extends Extension {
+    addBlocks(blocks: Blocks.BlockFactory): void {
+        blocks.registerBlock(new Blocks.Block(
+            'test', 'test %n', [10], Blocks.BlockType.Reporter,
+            'test'
+        ).addSpriteAction((x) => {
+            return x * 2;
+        }));
+    }
+    
     init() {
+        this.blocks.addCategory('test', new Color(255, 123, 0));
         console.log('initialized!');
-        console.log('ide', this.snap.IDE);
+        console.log('ide', Snap.IDE);
         this.events.addListener(new Events.Block.SnappedListener(args => {
             console.log(args.id);
         }));
